883b9df341daeb55abde873d54dbf8bbe083ebe0
Gyanni <50398872+Gyanni@users.noreply.github.com>
2022-05-12 15:07:13 +0900

    Create Lucky?.cpp

---
+++ Codeforces/#790/Lucky?.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ticket;
+int num[10];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	int t; cin >> t;
+
+	while (t--) {
+		cin >> ticket;
+		for (int i = 0; i < 6; i++) {
+			num[i] = ticket[i] - '0';
+		}
+
+		int k = num[0] + num[1] + num[2] - num[3] - num[4] - num[5];
+		if (k == 0) cout << "YES" << "\n";
+		else cout << "NO" << "\n";
+	}
+}
