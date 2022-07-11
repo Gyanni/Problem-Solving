fedde5cef33e7cf121e4a3c326c134c97bacc74e
Gyanni <50398872+Gyanni@users.noreply.github.com>
2022-07-11 11:27:51 +0900

    Create 1701B_Permutation.cpp

---
+++ Codeforces/ECR_131/1701B_Permutation.cpp
@@ -0,0 +1,35 @@
+// Author : Gyanni
+// Date : 2022-07-11
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int  main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	int t; cin >> t;
+	while (t--) {
+		cin >> n;
+
+		cout << 2 << "\n";
+
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 0) continue;
+
+			int k = i;
+			while (k <= n) {
+				cout << k << " ";
+				k *= 2;
+			}
+		}
+
+		cout << "\n";
+	}
+}
+
+// greedy
+// 
