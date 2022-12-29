155d2e5c44d84f6d2a5660c99a2499e3b18fc778
Gyanni <50398872+Gyanni@users.noreply.github.com>
2022-12-29 14:30:48 +0900

    Create 9656.cpp

---
+++ BOJ/9656.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+/**
+ * BOJ 9656
+ * 돌게임2
+ * 2022-12-29
+ * game theory
+ */
+
+using namespace std;
+using ll = long long;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+
+	ll n; cin >> n;
+	if (n % 2) cout << "CY" << "\n";
+	else cout << "SK" << "\n";
+}
